86ad60f358322344ed2fd23dc473f7380608c0f8
Mariia.Fadeeva <mariia.fadeeva@jetbrains.com>
2026-02-16 16:45:39 +0200

    Add task01 package with ROS2 receiver node and initial setup

---
+++ Practice02/src/task01/task01/receiver.py
@@ -0,0 +1,26 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class ReceiverNode(Node):
+    def __init__(self):
+        super().__init__('receiver')
+        self.subscription = self.create_subscription(
+            String,
+            '/spgc/sender',
+            self.listener_callback,
+            10)
+        self.subscription
+
+    def listener_callback(self, msg):
+        self.get_logger().info(f'I heard: "{msg.data}"')
+
+def main(args=None):
+    rclpy.init(args=args)
+    receiver_node = ReceiverNode()
+    rclpy.spin(receiver_node)
+    receiver_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
